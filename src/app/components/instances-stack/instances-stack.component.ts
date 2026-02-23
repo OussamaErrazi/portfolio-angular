4cb57e0ab670ff3ce3524f6c0189971413a5dd1c
ERRAZI <errazi111@gmail.com>
2026-02-23 16:19:06 +0000

    restyling instances stack

--- src/app/components/instances-stack/instances-stack.component.ts
+++ src/app/components/instances-stack/instances-stack.component.ts
@@ -1,16 +1,15 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { OpenInstance } from '../../models/OpenInstance';
-import { NgClass, NgFor, NgStyle } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { ContextMenuService } from '../../services/context-menu.service';
 
 @Component({
   selector: 'app-instances-stack',
-  imports: [NgFor, NgClass, NgStyle],
+  imports: [NgFor],
   templateUrl: './instances-stack.component.html',
   styleUrl: './instances-stack.component.scss'
 })
 export class InstancesStackComponent {
-  @ViewChild("instancesStack") instancesStack !: ElementRef<HTMLDivElement>;
   @Input() stack !:OpenInstance[];
   @Input() stackId !:string;
   @Input() removeStackElement !: (i: string, j: string)=> void;
@@ -24,17 +23,10 @@ export class InstancesStackComponent {
 
   hoverStart() {
     this.expand = true;
-    const el = this.instancesStack.nativeElement;
-    const expandedHeight = this.stack.length === 1 ? 24 : 10+ 24*this.stack.length + 5*(this.stack.length-1);
-    el.style.minHeight = `${expandedHeight}px`;
-    el.style.transform = `translateY(-${expandedHeight-24}px)`;
   }
 
   hoverEnd() {
     this.expand = false;
-    const el = this.instancesStack.nativeElement;
-    el.style.minHeight = "24px";
-    el.style.transform = `translateY(0)`;
   }
 
   hideReveal(itemId : string) {
